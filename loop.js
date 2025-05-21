5f818ec79624f3e1e849d4ca6ad46548aef20cb2
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-05-21 10:40:59 -0300

    loop

---
+++ loop.js
@@ -0,0 +1,19 @@
+// While =  Enquanto
+var x = 0;
+
+document.write("<h2>While </h2>");
+while(x < 10){ // Enquanto for menor que 10 ele vai satisfazer a condição
+    document.write("O valor de x é:  " + x + "<br>" ); // Inclui esse texto no meu html enquanto x for menor que 10
+
+    x++; // Incrementa o valor de x
+}
+
+
+// For = Para 
+document.write("<hr> <h2>For </h2>");
+
+var valor = 0;
+
+for(x1 = 10; x1 > valor; x1--){ //Vai decrementar até satisfazer a condição
+    document.write("O valor de x1 é:  " + x1 + "</br>");
+}
