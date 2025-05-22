099bae47f5fdc1138586cd593a406a32cd1d2793
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-05-22 12:08:52 -0300

    template strings

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+// tempate strings
+
+let nome = "stefany";
+let sobrenome = "Oliveira";
+let idade = 25;
+
+let mensagem = `meu nome é ${nome} ${sobrenome} e minha idade é ${idade}`;
+
+console.log(mensagem);
