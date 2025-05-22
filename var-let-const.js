c27be7a39fbe19807e8a42a9849b38f4940ff07f
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-05-22 11:42:23 -0300

    var, let e const

---
+++ var-let-const.js
@@ -0,0 +1,9 @@
+// Diferença entre var, let e const
+
+//var -> Você consegue acessar e alterar em qualquer lugar do código
+
+
+//let -> Você consegue acessar e alterar apenas dentro do escopo em que ele é criado
+
+
+//const -> Você consegue acessar  apenas dentro do escopo em que ele é criado e não é possível alterar. Tbm não é possível criar vazia
